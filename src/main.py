8c12e3f63296fdac30dd000d20130e5c88eb51c9
Erlend Elias Isachsen <erlend@isachsen.org>
2024-01-09 02:39:07 +0100

    - Improved assets - Added support for F and C clefs - Objects have better relative positions. - Fixed upside-down note. - Ledger line positions are better calculated. - Streak/score text moved to be less dependent on screen size.

--- src/main.py
+++ src/main.py
@@ -5,18 +5,22 @@ import random
 pygame.init()
 
 # Set up display
-width, height = 800, 600
+width, height = 1000, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Note Guessing Game")
 
 # Load images
 g_clef_image = pygame.image.load('assets/img/g_clef.png')
-quarter_note_image = pygame.image.load('assets/img/quarter_note_small.png')
+f_clef_image = pygame.image.load('assets/img/f_clef.png')
+c_clef_image = pygame.image.load('assets/img/c_clef.png')
+quarter_note_image = pygame.image.load('assets/img/quarter_note.png')
 quarter_note_correct_image = pygame.image.load('assets/img/quarter_note_correct.png')
 quarter_note_incorrect_image = pygame.image.load('assets/img/quarter_note_incorrect.png')
 flat_image = pygame.image.load('assets/img/flat.png')
 sharp_image = pygame.image.load('assets/img/sharp.png')
 ledger_line_image = pygame.image.load('assets/img/ledger_line.png')
+
+pixel_image = pygame.image.load('assets/img/pixel.png')
 
 # Define notes and their corresponding keys
 note_names = 'ABCDEFGH'
@@ -31,8 +35,21 @@ notes = [
     'C7', 'D7', 'E7', 'F7', 'G7', 'A7', 'B7',
     'C8'
 ]
+
+# Define accidentals and their corresponding probabilities
 accidentals = ['', 'b', '#']  # None, Flat, Sharp
 accidental_probabilities = [0.8, 0.1, 0.1]
+
+# Define clefs
+clefs = ['G', 'F', 'C']
+clef_images = [g_clef_image, f_clef_image, c_clef_image]
+clef_centernote = ['B4', 'D3', 'C4']
+clef_chosen = 'G'
+
+# Define note width, i.e. number of notes above and below the center note
+center_note = clef_centernote[clefs.index(clef_chosen)]
+#center_note = 'E6'
+note_width = 12
 
 # Initialize streak counter
 streak_counter = 0
@@ -47,7 +64,8 @@ running = True
 flipped = False
 previous_note = ''
 full_note = ''
-note_range = ['G3', 'D6']
+
+center_pos = (height // 2) - 3
 
 while running:
     screen.fill((255, 255, 255))
@@ -57,49 +75,50 @@ while running:
     
     # Ensure that the same note is not chosen twice in a row, and that a valid note is chosen
     while full_note == previous_note or full_note == '' or correct_note == '':
-        # Choose a random note within the range note_range
-        correct_note = random.choice(notes[notes.index(note_range[0]):notes.index(note_range[1]) + 1])
+        # Choose a random note based on the clef center note and note width
+        correct_note = random.choices(notes[notes.index(center_note) - note_width : notes.index(center_note) + note_width + 1])[0]
         accidental = random.choices(accidentals, accidental_probabilities)[0]
         full_note = correct_note + accidental
         
     previous_note = full_note
     
-    # If the note is C5 or higher, the quarter note should be mirrored vertically
-    if notes.index(correct_note) >= notes.index('B4') and not flipped:
-        quarter_note_image = pygame.transform.flip(quarter_note_image, False, True)
-        quarter_note_correct_image = pygame.transform.flip(quarter_note_correct_image, False, True)
-        quarter_note_incorrect_image = pygame.transform.flip(quarter_note_incorrect_image, False, True)
+    # If the note is C5 or higher, the quarter note should be rotated 180 degrees
+    if notes.index(correct_note) >= notes.index( clef_centernote[clefs.index(clef_chosen)] ) and not flipped:
+        quarter_note_image = pygame.transform.rotate(quarter_note_image, 180)
+        quarter_note_correct_image = pygame.transform.rotate(quarter_note_correct_image, 180)
+        quarter_note_incorrect_image = pygame.transform.rotate(quarter_note_incorrect_image, 180)
         flipped = True
-    elif notes.index(correct_note) < notes.index('B4') and flipped:
-        quarter_note_image = pygame.transform.flip(quarter_note_image, False, True)
-        quarter_note_correct_image = pygame.transform.flip(quarter_note_correct_image, False, True)
-        quarter_note_incorrect_image = pygame.transform.flip(quarter_note_incorrect_image, False, True)
+    elif notes.index(correct_note) < notes.index(clef_centernote[clefs.index(clef_chosen)]) and flipped:
+        quarter_note_image = pygame.transform.rotate(quarter_note_image, 180)
+        quarter_note_correct_image = pygame.transform.rotate(quarter_note_correct_image, 180)
+        quarter_note_incorrect_image = pygame.transform.rotate(quarter_note_incorrect_image, 180)
         flipped = False
         
-    # Display G-clef
-    screen.blit(g_clef_image, (width // 2 - g_clef_image.get_width() // 2, height // 2 - g_clef_image.get_height() // 2))
+    # Display Clef
+    clef_image = clef_images[clefs.index(clef_chosen)]
+    screen.blit(clef_image, (width // 2 - clef_image.get_width() // 2, height // 2 - clef_image.get_height() // 2))
 
     # Display quarter note at the correct position on the staff including octaves
-    note_position = (width // 2 - quarter_note_image.get_width() // 2, 244 - (notes.index(correct_note) - notes.index('E4')) * 24)
+    note_position = (width // 2 - quarter_note_image.get_width() // 2 + 100, center_pos + 24 - quarter_note_image.get_height() - (notes.index(correct_note) - notes.index( clef_centernote[clefs.index(clef_chosen)] )) * 24)
 
     # Display flat or sharp symbols based on the random modifier
     if accidental == 'b':
         screen.blit(flat_image, (note_position[0] - quarter_note_image.get_width() + flat_image.get_width() // 2, note_position[1] + quarter_note_image.get_height() // 2))
     elif accidental == '#':
-        screen.blit(sharp_image, (note_position[0] - quarter_note_image.get_width() + sharp_image.get_width() // 2, note_position[1] + quarter_note_image.get_height() // 2))
+        screen.blit(sharp_image, (note_position[0] - quarter_note_image.get_width() - 4, note_position[1] + sharp_image.get_height() + 5))
 
     # Add the correct ledger lines if the note is outside of the staff
     note_index = notes.index(correct_note)
     # Start with a note_offset of 0 if the note is C, A, F etc. Otherwise, start with a note_offset of 0
     note_offset = 1 if note_index % 2 == 0 else 0
-    while(note_index <= notes.index('C4')):
-        screen.blit(ledger_line_image, (note_position[0] - 8, note_position[1] + quarter_note_image.get_height() - 28 - 24 * note_offset ))
+    while(note_index <= notes.index( clef_centernote[clefs.index(clef_chosen)] ) - 6):
+        screen.blit(ledger_line_image, (note_position[0] - 12, note_position[1] + quarter_note_image.get_height() - 28 - 24 * note_offset ))
         note_offset += 2
         note_index += 2
-    if(notes.index(correct_note) >= notes.index('A5')):
-        note_index = notes.index('A5')
+    if(notes.index(correct_note) >= notes.index( clef_centernote[clefs.index(clef_chosen)] ) + 6):
+        note_index = notes.index( clef_centernote[clefs.index(clef_chosen)] ) + 6
         while(note_index <= notes.index(correct_note)):
-            screen.blit(ledger_line_image, (note_position[0] - 8, note_position[1] + quarter_note_image.get_height() - 28  + 24 * note_offset ))
+            screen.blit(ledger_line_image, (note_position[0] - 12, note_position[1] + quarter_note_image.get_height() - 28  + 24 * note_offset ))
             note_offset += 2
             note_index += 2
 
@@ -139,8 +158,8 @@ while running:
     
     streak_text = font.render("Streak: {}".format(streak_counter), True, text_color)
     highest_streak_text = font.render("Highest: {}".format(highest_streak), True, black_color)
-    screen.blit(streak_text, (10, 10))
-    screen.blit(highest_streak_text, (10, 50))
+    screen.blit(streak_text, (23, center_pos - 20))
+    screen.blit(highest_streak_text, (10, center_pos + 20))
 
     # Update the display
     pygame.display.flip()
